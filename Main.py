45d6d92bd8e3ee83ce7c72d8eedc4cb78940b806
amreyer <amreyer2@gmail.com>
2016-10-11 20:14:03 -0700

    Added a region menu

--- Main.py
+++ Main.py
@@ -2,9 +2,31 @@ from RiotAPI import RiotAPI
 import RiotConsts as Consts
 
 def main():
-    #summoner = raw_input()
 
-    api = RiotAPI(Consts.KEY)
+    regionMenu()
+
+    while(True):
+        choice = input('Enter the number of your region (1-5): ')
+        if(1<=choice<=5):
+            break
+        else:
+            print 'ERROR: Invalid Region Choice'
+
+    region = Consts.REGIONS['north_america']
+
+    if(choice==1):
+        region = Consts.REGIONS['north_america']
+    elif(choice==2):
+        region = Consts.REGIONS['europe_west']
+    elif(choice==3):
+        region = Consts.REGIONS['europe_nordic_and_east']
+    elif(choice==4):
+        region = Consts.REGIONS['korea']
+    elif(choice==5):
+        region = Consts.REGIONS['brazil']
+
+    api = RiotAPI(Consts.KEY, region)
+
 
     r = api.get_summoner_by_name('Anguriel')
     sumID = r['anguriel']['id']
@@ -31,6 +53,15 @@ def main():
 
     #print league[str(sumID)][0]
 
+def regionMenu():
+    print '1. North America \n' \
+          '2. European West \n' \
+          '3. European and Nordic East \n' \
+          '4. Korea \n' \
+          '5. Brazil \n'
+
+
 
 if __name__ == "__main__":
     main()
+
